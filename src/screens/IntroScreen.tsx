4d4b12842faa48ccb0ae4934ccaa9d5200fa90a3
Adel Rodriguez <adelrodriguez@Adels-MacBook-Pro.local>
2020-03-20 00:14:12 -0400

    feat: complete IntroScreen

--- src/screens/IntroScreen.tsx
+++ src/screens/IntroScreen.tsx
@@ -1,16 +1,63 @@
-import React from 'react';
-import { Button, Header } from 'semantic-ui-react';
+import React, { useEffect } from 'react';
+import {
+  Transition,
+  Button,
+  Header,
+  Icon,
+  Container,
+  Loader,
+} from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import { useStoreState, useStoreActions } from '../hooks';
 
 export function IntroScreen() {
+  const loading = useStoreState(state => state.questions.loading);
+  const error = useStoreState(state => state.questions.error);
+
+  const { getQuestions } = useStoreActions(actions => actions.questions);
+
+  useEffect(() => {
+    getQuestions();
+  }, [getQuestions]);
+
+  if (loading) {
+    return (
+      <div>
+        <Loader active>Loading</Loader>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <Transition animation="fade up" duration={1000} transitionOnMount>
+        <Header size="huge" textAlign="center" icon>
+          <Icon name="warning" />
+          Error
+          <Header.Subheader>
+            There was an error while loading the questions. Please refresh the
+            page.
+          </Header.Subheader>
+        </Header>
+      </Transition>
+    );
+  }
+
   return (
-    <>
-      <Header>Welcome to the Trivia Challenge!</Header>
-      <p>You will be presented 10 True of False questions.</p>
-      <p>Can you score 100%?</p>
-      <Link to="/quiz">
-        <Button>BEGIN</Button>
-      </Link>
-    </>
+    <Transition animation="fade up" duration={1000} transitionOnMount>
+      <Container textAlign="center">
+        <Header size="huge" textAlign="center" icon>
+          <Icon name="question circle outline" />
+          Welcome to the Trivia Challenge!
+          <Header.Subheader>
+            You will be presented 10 True of False questions.
+          </Header.Subheader>
+        </Header>
+        <p>Can you score 100%?</p>
+        <Link to="/quiz">
+          <Button>BEGIN</Button>
+        </Link>
+      </Container>
+    </Transition>
   );
 }
