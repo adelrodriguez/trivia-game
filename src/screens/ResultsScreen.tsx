83045c1aadf94e881a55c087b586b05f28d862aa
Adel Rodriguez <adelrodriguez@Adels-MacBook-Pro.local>
2020-03-20 00:14:25 -0400

    feat: complete ResultsScreen

--- src/screens/ResultsScreen.tsx
+++ src/screens/ResultsScreen.tsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import { Header, Button, Transition } from 'semantic-ui-react';
+import { Redirect, Link } from 'react-router-dom';
+import { useStoreState, useStoreActions } from '../hooks';
+import { ResultsList } from '../components/ResultsList';
 
 export function ResultsScreen() {
-  return <div>This is the Results Screen</div>;
+  const score = useStoreState(state => state.questions.score);
+  const results = useStoreState(state => state.questions.results);
+  const { reset } = useStoreActions(actions => actions.questions);
+
+  if (!results.length) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <Transition animation="fade up" duration={1000} transitionOnMount>
+      <div>
+        <Header size="huge" textAlign="center">
+          You scored
+          <Header.Subheader>
+            {score}/{results.length}
+          </Header.Subheader>
+        </Header>
+        <ResultsList results={results} />
+        <Link to="/">
+          <Button fluid onClick={() => reset()}>
+            PLAY AGAIN?
+          </Button>
+        </Link>
+      </div>
+    </Transition>
+  );
 }
