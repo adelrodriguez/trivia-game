6946212b145e5b0441ed0619a1711bacf2db291c
Adel Rodriguez <adelrodriguez@Adels-MacBook-Pro.local>
2020-03-20 00:21:10 -0400

    Updates

---
+++ precache-manifest.abc9e4dd44994823e7c1bd378a45f89b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef71a81742c71e9f1d8c864630d5e34",
+    "url": "/trivia-game/index.html"
+  },
+  {
+    "revision": "e91320b43ca527241cda",
+    "url": "/trivia-game/static/css/2.1d9e2010.chunk.css"
+  },
+  {
+    "revision": "eff59d75b21ffa6934f6",
+    "url": "/trivia-game/static/css/main.58602fd4.chunk.css"
+  },
+  {
+    "revision": "e91320b43ca527241cda",
+    "url": "/trivia-game/static/js/2.a988108e.chunk.js"
+  },
+  {
+    "revision": "2c7a5dc1a2ba489b60fc0ac69b399b45",
+    "url": "/trivia-game/static/js/2.a988108e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff59d75b21ffa6934f6",
+    "url": "/trivia-game/static/js/main.a0574e5f.chunk.js"
+  },
+  {
+    "revision": "86738fa43ed2dc363826",
+    "url": "/trivia-game/static/js/runtime-main.a315b3d2.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/trivia-game/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/trivia-game/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/trivia-game/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/trivia-game/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/trivia-game/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/trivia-game/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/trivia-game/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/trivia-game/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/trivia-game/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/trivia-game/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/trivia-game/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/trivia-game/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/trivia-game/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/trivia-game/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/trivia-game/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/trivia-game/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
